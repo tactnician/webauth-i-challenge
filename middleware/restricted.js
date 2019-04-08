9768b5aa717b3b03ee36757cfa8205140e140cc7
tactnician <tactnician@gmail.com>
2019-04-08 18:35:13 -0400

    added login, register, get users routes and restricted middleware

---
+++ middleware/restricted.js
@@ -0,0 +1,23 @@
+function restricted(req, res, next) {
+    const {username, password} = req.headers;
+    
+        if(username && password) {
+        Users.findBy({username})
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+            next()
+            } else {
+            res.status(401).json({message: 'Invalid Credentials'})
+            }
+        }).catch(error => {
+            res.status(500).json({
+                message: 'Something went wrong on our end.', error: error
+        })
+        })
+        } else {
+            res.status(400).json({message: 'No credentials provided.'})
+        } 
+}
+
+module.exports = restricted;
