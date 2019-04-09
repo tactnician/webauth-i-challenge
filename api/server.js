c32b87196441fb3528058a71395ffd49fb83a544
tactnician <tactnician@gmail.com>
2019-04-09 16:39:44 -0400

    added dependencies for sessions and cookies

--- api/server.js
+++ api/server.js
@@ -2,15 +2,40 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 // const restricted = require('../middleware/restricted');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+const dbConfig = require('../data/dbConfig');
 
 const router = require('../router/router');
 
-server = express();
+const server = express();
+
+sessionConfig = {
+    name: 'cookieID',
+    secret: process.env.SECRET || "monkey-pants",
+    cookie: {
+        maxAge: 1000*60*60,
+        secure: false,
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: false,
+    store: new KnexSessionStore({
+        knex: dbConfig,
+        tablename: 'sessions',
+        sidfieldname: 'sid',
+        createtable: true,
+      clearInterval: 1000 * 60 *10
+    })
+}
 
 server.use(express.json())
 server.use(helmet());
-server.use(cors('headers'));
+server.use(session(sessionConfig));
+// server.use(cors('headers'));
 // server.use(restricted());
+
+server.use('/api', router);
 
 server.get('/', (req, res) => {
     res.send(`
@@ -18,6 +43,6 @@ server.get('/', (req, res) => {
     `)
 })
 
-server.use('/api', router);
+
 
 module.exports = server; 
--- middleware/restricted.js
+++ middleware/restricted.js
@@ -1,27 +1,41 @@
 
-const Auth = require('../router/router-helper')
-
-function restricted(req, res, next) {
-    const {username, password} = req.headers;
-    
-        if(username && password) {
-        Auth.findBy({username})
-        .first()
-        .then(user => {
-            if (user && bcrypt.compareSync(password, user.password)) {
+module.exports = (req, res, next) => {
+    try {
+        if (req.session && req.session.user){
             next()
-            } else {
-            res.status(401).json({message: 'Invalid Credentials'})
-            }
-        }).catch(error => {
-            console.log(error)
-            res.status(500).json({
-                message: 'Something went wrong on our end.', error: error
-            })
-        })
         } else {
-            res.status(400).json({message: 'No credentials provided.'})
-        } 
-}
+            res.status(401).json({message: `You shall not pass`})
+        }
+        } catch (error) {
+        res.status(500).json({message:`error`};)
+        }
+    
+};
 
-module.exports = restricted;
+
+// const Auth = require('../router/router-helper')
+
+// function restricted(req, res, next) {
+//     const {username, password} = req.headers;
+    
+//         if(username && password) {
+//         Auth.findBy({username})
+//         .first()
+//         .then(user => {
+//             if (user && bcrypt.compareSync(password, user.password)) {
+//             next()
+//             } else {
+//             res.status(401).json({message: 'Invalid Credentials'})
+//             }
+//         }).catch(error => {
+//             console.log(error)
+//             res.status(500).json({
+//                 message: 'Something went wrong on our end.', error: error
+//             })
+//         })
+//         } else {
+//             res.status(400).json({message: 'No credentials provided.'})
+//         } 
+// }
+
+// module.exports = restricted;
